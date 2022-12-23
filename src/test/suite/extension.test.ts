2884dc02ea8f76f2234be4766bc2f009e0ed02b9
Daniel Ciborowski <dciborow@microsoft.com>
2022-12-23 08:35:11 +0000

    Adding Test Cases

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,6 +3,8 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import LineData from '../../LineData';
+import { getLineMatch } from '../../operatorGroups';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
@@ -12,4 +14,47 @@ suite('Extension Test Suite', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
+
+	const lineMatch = getLineMatch();
+
+	const case1 = 'param isZoneRedundant bool = false';
+	const test1 = LineData.fromString(case1);
+	test('Test Parameters', () => {
+		assert.strictEqual(test1.prefix, '');
+		assert.strictEqual(test1.parts[0].text, 'param isZoneRedundant ');
+		assert.strictEqual(test1.parts[0].operator, 'bool');
+		assert.strictEqual(test1.parts[0].operatorType, 'types');
+		assert.strictEqual(test1.parts[1].operator, '=');
+		assert.strictEqual(test1.parts[1].operatorType, 'assignment');
+	});
+
+	const case2 = 'var certificateIssuer = \'Subscription-Issuer\'';
+	const test2 = LineData.fromString(case2);
+	test('Test Variables', () => {
+		assert.strictEqual(test2.prefix, '');
+		assert.strictEqual(test2.parts[0].text, 'var certificateIssuer ');
+		assert.strictEqual(test2.parts[0].operator, '=');
+		assert.strictEqual(test2.parts[0].operatorType, 'assignment');
+	});
+
+	const case3 = "resource userAssignedIdentity 'Microsoft.ManagedIdentity/userAssignedIdentities@2018-11-30' = { name: '\${prefix}-id', location: location }";
+	const test3 = LineData.fromString(case3);
+	test('Test Resources', () => {
+		assert.strictEqual(test3.prefix, '');
+		assert.strictEqual(test3.parts[0].text, "resource userAssignedIdentity 'Microsoft.ManagedIdentity/userAssignedIdentities@2018-11-30' ");
+		assert.strictEqual(test3.parts[0].operator, '=');
+		assert.strictEqual(test3.parts[0].operatorType, 'assignment');
+	});
+
+	const case4 = 'output appName string = appName';
+	const test4 = LineData.fromString(case4);
+	test('Test Output', () => {
+		assert.strictEqual(test4.prefix, '');
+		assert.strictEqual(test4.parts[0].text, 'output appName ');
+		assert.strictEqual(test4.parts[0].operator, 'string');
+		assert.strictEqual(test4.parts[0].operatorType, 'types');
+		assert.strictEqual(test4.parts[1].operator, '=');
+		assert.strictEqual(test4.parts[1].operatorType, 'assignment');
+	});
+
 });
